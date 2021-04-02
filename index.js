9c11a1fb4fd5e810438ea33105674ed390b928a8
iffatunnessa <45840519+iffatunnessa@users.noreply.github.com>
2021-04-02 23:37:15 +0600

    new

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const MongoClient = require('mongodb').MongoClient;
 const { connect } = require('mongodb');
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 const port = 5000
@@ -18,17 +19,49 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   console.log('connection err', err);
   const itemCollection = client.db("foodAnywhere").collection("items");
+  const orderedItemCollection = client.db("foodAnywhere").collection("orderedItems");
   //get
   app.get('/home', (req, res) => {
     itemCollection.find()
-    .toArray((err, items)=>{
-      // console.log(items);
-      res.send(items);
-    })
+      .toArray((err, items) => {
+        // console.log(items);
+        res.send(items);
+      })
+  })
+
+  app.get('/checkout/:id', (req, res) => {
+    itemCollection.find({ _id: ObjectID(req.params.id) })
+      .toArray((err, items) => {
+        res.send(items);
+      })
+  })
+
+  app.get('/orders', (req, res) => {
+    // console.log(req.query.email);
+    orderedItemCollection.find({email:req.query.email})
+      .toArray((err, items) => {
+        res.send(items);
+      })
+  })
+
+  app.get('/admin/manageItems', (req, res) => {
+    itemCollection.find()
+      .toArray((err, items) => {
+        res.send(items);
+      })
+  })
+  //delete 
+  app.delete('/admin/manageItems/:id', (req, res) => {
+    console.log("delete",req.params.id);
+    itemCollection.findOneAndDelete({ _id: ObjectID(req.params.id)})
+      .then( items => {
+        res.send(items);
+        console.log(items)
+      })
   })
 
   // post
-  app.post('/addItems', (req, res) => {
+  app.post('/admin/addItems', (req, res) => {
     const newItem = req.body;
     itemCollection.insertOne(newItem)
       .then(result => {
@@ -38,9 +71,18 @@ client.connect(err => {
       })
     console.log('add', newItem);
   })
+
+  app.post('/checkout/:id', (req, res) => {
+    const newItem = req.body;
+    orderedItemCollection.insertOne(newItem)
+      .then(result => {
+        console.log("inserted count", result.insertedCount)
+        res.send(result.insertedCount > 0)
+        console.log('res', result);
+      })
+    console.log('add', newItem);
+  })
 });
-
-
 
 
 app.listen(port);
